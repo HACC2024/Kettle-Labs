3c85d4f77683caaa112b68f55d13d4e7127900bc
Bryan Estrada <bryan@fanrock.com>
2024-11-06 14:03:00 -1000

    checkpoint progress on trigger custom events

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,23 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
+export {};
+declare global {
+  interface Window {
+    dataLayer: any[]
   }
-});
+}
+console.log("HACC24");
+
+// parse the current URL and query string
+const a = document.createElement('a');
+a.href = window.location.href;
+
+const tokens = a.pathname.split('/').filter(token => token.length);
+const search = new URLSearchParams(a.search);
+
+
+/** fire a custom event on searches */
+if (1 === tokens.length && tokens[0] === 'dataset' && search.has('q')) {
+  window.dataLayer.push({
+    event: 'search',
+    search_term: search.get('q')
+  });
+}
