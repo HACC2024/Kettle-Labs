e33b2c4fc05f35236301fed75eb0ed9b7f4c2005
Bryan Estrada <bryan@fanrock.com>
2024-11-07 13:47:39 -1000

    add options to save openai token remove boilerplate for unused popup

--- src/options.tsx
+++ src/options.tsx
@@ -1,36 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 
+
 const Options = () => {
-  const [color, setColor] = useState<string>("");
+  const [secretKey, setSecretKey] = useState<string>("");
+  const [organization, setOrganization] = useState<string>("");
   const [status, setStatus] = useState<string>("");
-  const [like, setLike] = useState<boolean>(false);
+  
 
   useEffect(() => {
     // Restores select box and checkbox state using the preferences
     // stored in chrome.storage.
-    chrome.storage.sync.get(
+    chrome.storage.local.get(
       {
-        favoriteColor: "red",
-        likesColor: true,
+        secretKey: "",
+        organization: ""
       },
       (items) => {
-        setColor(items.favoriteColor);
-        setLike(items.likesColor);
+        setSecretKey(items.secretKey);
+        setOrganization(items.organization);
       }
     );
   }, []);
 
   const saveOptions = () => {
-    // Saves options to chrome.storage.sync.
-    chrome.storage.sync.set(
-      {
-        favoriteColor: color,
-        likesColor: like,
-      },
+    // Saves options to chrome.storage.local.
+    chrome.storage.local.set( { secretKey, organization },
       () => {
         // Update status to let user know options were saved.
-        setStatus("Options saved.");
+        setStatus("Options synchronized.");
         const id = setTimeout(() => {
           setStatus("");
         }, 1000);
@@ -40,36 +38,27 @@ const Options = () => {
   };
 
   return (
-    <>
-      <div>
-        Favorite color: <select
-          value={color}
-          onChange={(event) => setColor(event.target.value)}
-        >
-          <option value="red">red</option>
-          <option value="green">green</option>
-          <option value="blue">blue</option>
-          <option value="yellow">yellow</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={like}
-            onChange={(event) => setLike(event.target.checked)}
-          />
-          I like colors.
-        </label>
-      </div>
+    <form onSubmit={saveOptions}>
+      <fieldset>
+        <legend>enter your OpenAI credentials</legend>
+        <div>
+          <label htmlFor="secret">Secret Key: </label>
+          <input type="text" name="secret" id="secret" placeholder="OpenAI Secret Key" onChange={e => setSecretKey(e.currentTarget.value)} value={secretKey}></input>
+        </div>
+        <div>
+          <label htmlFor="org">Organization: </label>
+          <input type="text" name="org" id="org" placeholder="OpenAI Organization" onChange={e => setOrganization(e.currentTarget.value)} value={organization}></input>
+        </div>
+      </fieldset>
       <div>{status}</div>
-      <button onClick={saveOptions}>Save</button>
-    </>
+      <input type="submit" value="Save Options" />
+    </form>
   );
 };
 
-const root = createRoot(document.getElementById("root")!);
-
+//const root = createRoot(document.getElementById("root")!);
+//const root:Element = document.getElementById("root") as Element;
+const root = createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Options />
